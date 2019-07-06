a39678e042dc696b4d78fbb651039b4d8bff3fd7
duzj <dzj0574@163.com>
2019-07-06 14:37:40 +0800

    :pig: 修改了test

--- sql/test.sql
+++ sql/test.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50724
  File Encoding         : 65001
 
- Date: 06/07/2019 13:45:42
+ Date: 06/07/2019 14:18:54
 */
 
 SET NAMES utf8mb4;
@@ -25,14 +25,21 @@ CREATE TABLE `test`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `age` int(11) NULL DEFAULT NULL,
+  `createDate` datetime(0) NULL DEFAULT NULL,
+  `createBy` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `updateDate` datetime(0) NULL DEFAULT NULL,
+  `updateBy` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `memo1` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `memo2` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `memo3` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of test
 -- ----------------------------
-INSERT INTO `test` VALUES (1, '关羽', 18);
-INSERT INTO `test` VALUES (2, '刘备', 20);
-INSERT INTO `test` VALUES (3, '张飞', 19);
+INSERT INTO `test` VALUES (1, '关羽', 18, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `test` VALUES (2, '刘备', 20, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `test` VALUES (3, '张飞', 19, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 SET FOREIGN_KEY_CHECKS = 1;
